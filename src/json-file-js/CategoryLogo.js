e85401b58d6674649ae2c0c6bed67b6776e0de6c
deepak bhardwaj <115878578+deepakbhardwajvst@users.noreply.github.com>
2023-10-18 15:07:41 +0530

    Add files via upload

---
+++ src/json-file-js/CategoryLogo.js
@@ -0,0 +1,112 @@
+export const links = [
+  {
+    label: "Amazing",
+    imgSrc:
+      "https://a0.muscache.com/pictures/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg",
+  },
+  {
+    label: "Top of the world",
+    imgSrc:
+      "https://a0.muscache.com/pictures/248f85bf-e35e-4dc3-a9a1-e1dbff9a3db4.jpg",
+  },
+  {
+    label: "Farms",
+    imgSrc:
+      "https://a0.muscache.com/pictures/aaa02c2d-9f0d-4c41-878a-68c12ec6c6bd.jpg",
+  },
+  {
+    label: "Surfing",
+    imgSrc:
+      "https://a0.muscache.com/pictures/957f8022-dfd7-426c-99fd-77ed792f6d7a.jpg",
+  },
+  {
+    label: "Lakefront",
+    imgSrc:
+      "https://a0.muscache.com/pictures/677a041d-7264-4c45-bb72-52bff21eb6e8.jpg",
+  },
+  {
+    label: "Rooms",
+    imgSrc:
+      "https://a0.muscache.com/pictures/7630c83f-96a8-4232-9a10-0398661e2e6f.jpg",
+  },
+  {
+    label: "Beach",
+    imgSrc:
+      "https://a0.muscache.com/pictures/10ce1091-c854-40f3-a2fb-defc2995bcaf.jpg",
+  },
+  {
+    label: "OMG!",
+    imgSrc:
+      "https://a0.muscache.com/pictures/c5a4f6fc-c92c-4ae8-87dd-57f1ff1b89a6.jpg",
+  },
+  {
+    label: "Lakefront",
+    imgSrc:
+      "https://a0.muscache.com/pictures/3726d94b-534a-42b8-bca0-a0304d912260.jpg",
+  },
+  {
+    label: "Cabins",
+    imgSrc:
+      "https://a0.muscache.com/pictures/732edad8-3ae0-49a8-a451-29a8010dcc0c.jpg",
+  },
+  {
+    label: "Lakefront",
+    imgSrc:
+      "https://a0.muscache.com/pictures/c5a4f6fc-c92c-4ae8-87dd-57f1ff1b89a6.jpg",
+  },
+  {
+    label: "Countryside",
+    imgSrc:
+      "https://a0.muscache.com/pictures/6ad4bd95-f086-437d-97e3-14d12155ddfe.jpg",
+  },
+  {
+    label: "Historical homes",
+    imgSrc:
+      "https://a0.muscache.com/pictures/33dd714a-7b4a-4654-aaf0-f58ea887a688.jpg",
+  },
+  {
+    label: "Beachfront",
+    imgSrc:
+      "https://a0.muscache.com/pictures/bcd1adc0-5cee-4d7a-85ec-f6730b0f8d0c.jpg",
+  },
+  {
+    label: "Castles",
+    imgSrc:
+      "https://a0.muscache.com/pictures/1b6a8b70-a3b6-48b5-88e1-2243d9172c06.jpg",
+  },
+  {
+    label: "Trending",
+    imgSrc:
+      "https://a0.muscache.com/pictures/3726d94b-534a-42b8-bca0-a0304d912260.jpg",
+  },
+  {
+    label: "Caves",
+    imgSrc:
+      "https://a0.muscache.com/pictures/4221e293-4770-4ea8-a4fa-9972158d4004.jpg",
+  },
+  {
+    label: "New",
+    imgSrc:
+      "https://a0.muscache.com/pictures/c0fa9598-4e37-40f3-b734-4bd0e2377add.jpg",
+  },
+  {
+    label: "Design",
+    imgSrc:
+      "https://a0.muscache.com/pictures/50861fca-582c-4bcc-89d3-857fb7ca6528.jpg",
+  },
+  {
+    label: "Lakefront",
+    imgSrc:
+      "https://a0.muscache.com/pictures/c5a4f6fc-c92c-4ae8-87dd-57f1ff1b89a6.jpg",
+  },
+  {
+    label: "Islands",
+    imgSrc:
+      "https://a0.muscache.com/pictures/8e507f16-4943-4be9-b707-59bd38d56309.jpg",
+  },
+  {
+    label: "Luxe",
+    imgSrc:
+      "https://a0.muscache.com/pictures/c8e2ed05-c666-47b6-99fc-4cb6edcde6b4.jpg",
+  },
+];
